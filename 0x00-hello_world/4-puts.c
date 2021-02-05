9a079d36037a1a0ddb54138ed6330ab782296f99
zidanegimiga <zidanegimiga@gmail.com>
2021-02-05 20:25:08 +0000

    Printing without printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
-*main - Entry point
-*Return: 0 (Success)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
+	char test_str[] = "\"Programming is like building a multingual puzzle";
 
-puts("\Programming is like building a multilingual puzzle");
-return (0);
-
+	puts(test_str);
+	return (0);
 }
- 
