5d21c4dab9ab17badaeca4130cd53d8da2cc9778
zidanegimiga <zidanegimiga@gmail.com>
2021-02-05 10:53:54 +0000

    A c program that prints numbers from 0-10 using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 15;
+
+	while (a < 25)
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+
+	return (0);
+}
