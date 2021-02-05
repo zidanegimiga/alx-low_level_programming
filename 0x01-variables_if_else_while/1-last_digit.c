bf23de63dc9006beef146d2552f64f2b2c5fbba6
zidanegimiga <zidanegimiga@gmail.com>
2021-02-05 06:58:54 +0000

    Removed double files and pushed a new c program that assigns a random number to the variable n each time it is executed

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,24 +2,32 @@
 #include <time.h>
 #include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+/*
+ * main - it returns the digit 0
+ * Return - Always 0
  */
+
 int main(void)
 {
-        int n, last_d;
+	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
- 	last_d = n % 10;
-	if (last_d > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, last_d);
-	else if (last_d == 0)
-		printf("Last digit of %d is %d and is 0\n", n, last_d);
+
+	printf("Last digit of %i is %i and is ", n, (n % 10));
+
+	if ((n % 10) == 0)
+	{
+		printf("0\n");
+	}
+	else if ((n % 10) > 5)
+	{
+		printf("greater than 5\n");
+	}
 	else
-                printf("Last digit of %d is %d and is less than 6 and is not 0\n", n, last_d); 
+	{
+		printf("less than 6 and not 0\n");
+	} 
 	return (0);
 }
-
+ 
