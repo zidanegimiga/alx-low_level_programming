df940489806d084ae3328417c1e93535011b7b87
zidanegimiga <zidanegimiga@gmail.com>
2021-02-09 20:35:29 +0000

    Pushing all of mt files to the repo

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * add - adds two integers and returns the sum
+ * @n: added to m
+ * @m: added to n
+ *
+ * Return: returns the sum
+ */
+int add(int n, int m)
+{	int sum;
+
+	sum = m + n;
+	return (sum);
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - prints every minute of the day of JB
+ *
+ * Return: does not return anything
+ */
+void jack_bauer(void)
+{
+	int h, m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			_putchar('0' + (h / 10));
+			_putchar('0' + (h % 10));
+			_putchar(':');
+			_putchar('0' + (m / 10));
+			_putchar('0' + (m % 10));
+			_putchar('\n');
+		}
+	}
+}
