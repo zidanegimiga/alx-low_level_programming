f09b778f736f1f6847e3119efd3f67f308deb32b
Zidane Gimiga <33351058+zidanegimiga@users.noreply.github.com>
2021-02-11 23:50:57 +0300

    A function that prints an integer.
    Numbers have life; they're not just symbols on paper
    Prototype: void print_number(int n);

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,1 +1,52 @@
-dfghjk
+#include "holberton.h"
+
+/**
+ * print_number - prints a number using _putchar
+ * @n: number to print
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	if (n >= 0)
+	{
+		num_to_char(n);
+	}
+	else
+	{
+		n = -n;
+		_putchar('-');
+		num_to_char(n);
+	}
+}
+
+/**
+ * num_to_char - transforms a number with 1 or more digits into a char
+ * @n: number to print
+ *
+ * Return: void
+ */
+void num_to_char(int n)
+{
+	int d = 10;
+
+	if (n < d)
+	{
+		_putchar('0' + n);
+	}
+	else
+	{
+		while (n >= d)
+		{
+			d *= 10;
+		}
+		d /= 10;
+		_putchar('0' + n / d);
+		while (d != 10)
+		{
+			d /= 10;
+			_putchar('0' + (n / d) % 10);
+		}
+		_putchar('0' + n % 10);
+	}
+}
