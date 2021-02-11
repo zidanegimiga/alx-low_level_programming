0a09dcc5ecb0101a62e24a8c5727a14bea6d2d4b
Zidane Gimiga <33351058+zidanegimiga@users.noreply.github.com>
2021-02-11 12:01:44 +0300

    A function that checks for the uppercase character.
    Prototype: int _isupper(int c);
    Returns 1 if c is uppercase
    Returns 0 otherwise

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,1 +1,15 @@
-sdfghjkl;
+#include "holberton.h"
+
+/**
+ * _isupper - returns whether a character's ASCII value is uppercase
+ * @c: ASCII character to be checked
+ *
+ * Return: 1 if it is uppercase and 0 if it isn't
+ */
+int _isupper(int c)
+{
+  if (c >= 65 && c <= 90)
+    return (1);
+  else
+    return (0);
+}
