6c0a51acf1ac409d2363e7b1cda6fd310a8790b7
Zidane Gimiga <33351058+zidanegimiga@users.noreply.github.com>
2021-02-11 12:12:17 +0300

    a function that draws a straight line in the terminal.
    Prototype: void print_line(int n);
    Where n is the number of times the character _ should be printed
    The line should end with a \n
    If n is 0 or less, the function should only print \n

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,1 +1,22 @@
-ERTYUIOP
+#include "holberton.h"
+
+/**
+ * print_line - prints line using "_" character of n length
+ *@n: length of the line
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i = 0;
+
+	if (n > 0)
+	{
+		while (i < n)
+		{
+			_putchar(95);
+			i++;
+		}
+	}
+	_putchar('\n');
+}
