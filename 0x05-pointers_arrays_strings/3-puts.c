20053f73b2d57f230e98dfdb5ec26a0805d27c50
Zidane Gimiga <33351058+zidanegimiga@users.noreply.github.com>
2021-02-17 15:58:04 +0300

    a function that prints a string, followed by a new line, to stdout
    Prototype: void _puts(char *str);

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,1 +1,19 @@
-dfghjkl
+#include "holberton.h"
+
+/**
+ * _puts - print a given string to screen
+ *
+ * @str: passed pointer argument for string
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int len = 0;
+
+	while (*(str + len) != '\0')
+	{
+		_putchar(*(str + len));
+		len++;
+	}
+	_putchar('\n');
+}
