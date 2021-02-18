dad61add8720dd64ad7b7a4e5c1dc0db5daa20d3
Zidane Gimiga <33351058+zidanegimiga@users.noreply.github.com>
2021-02-18 22:54:10 +0300

    ROT13-program
    a function that encodes a string using rot13.
    
    Prototype: char *rot13(char *);
    You can only use if statement once in your code
    You can only use two loops in your code

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,1 +1,39 @@
-dtfyguhij
+#include "holberton.h"
+
+/**
+ * rot13 - converts string to leet
+ *
+ * @c: character string pointer
+ * Return: char pointer
+ */
+char *rot13(char *c)
+{
+	int i, j;
+	char rot_13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm",
+	alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (alphabet[j] == c[i])
+			{
+				c[i] = rot_13[j];
+				break;
+			}
+		}
+	}
+	return (c);
+}
+© 2021 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
