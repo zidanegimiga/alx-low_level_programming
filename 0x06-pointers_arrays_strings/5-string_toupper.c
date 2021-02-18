370316044fb0bac6214988fddddbb619b5f4eb83
Zidane Gimiga <33351058+zidanegimiga@users.noreply.github.com>
2021-02-18 22:47:41 +0300

    a function that reverses the content of an array of integers.

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,1 +1,20 @@
-ertfyhjmk,l;p
+#include "holberton.h"
+
+/**
+ * string_toupper - function to transform all lowercase in string to uppercase
+ *
+ * @c: character string pointer
+ * Return: char pointer
+ */
+char *string_toupper(char *c)
+{
+	int i = 0;
+
+	while (c[i] != '\0')
+	{
+		if (c[i] > 96 && c[i] < 123)
+			c[i] -= 32;
+		i++;
+	}
+	return (c);
+}
