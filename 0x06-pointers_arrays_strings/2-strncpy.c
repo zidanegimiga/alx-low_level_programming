40ec0cc03a8891a47e5b8bc618bba045096f14d2
Zidane Gimiga <33351058+zidanegimiga@users.noreply.github.com>
2021-02-18 22:42:21 +0300

    A function that concanates a string
    Prototype: char *_strncat(char *dest, char *src, int n);
    The _strncat function is similar to the _strcat function, except that
    it will use at most n bytes from src; and
    src does not need to be null-terminated if it contains n or more bytes
    Return a pointer to the resulting string dest

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,1 +1,20 @@
-edrtyhuikl;
+#include "holberton.h"
+
+/**
+ * _strncpy - function to copy string
+ *
+ * @dest: string to copy to
+ * @src: string to copy from
+ * @n: number of chars to copy
+ * Return: char pointer
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	while (i < n)
+		dest[i++] = '\0';
+	return (dest);
+}
