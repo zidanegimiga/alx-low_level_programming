61d4ad9c145f823ba9ac210d20e9f1f79203f959
Zidane Gimiga <33351058+zidanegimiga@users.noreply.github.com>
2021-02-19 22:52:18 +0300

    A program that prints an integer using _putchar

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -1,18 +1,42 @@
 #include "holberton.h"
 
 /**
- * print_number - function to concatenate two strings and return a new string
+ * print_number - function to print int using _putchar
  *
- * Return: null
+ * @num: int to print
  */
-void print_number(int n)
+void print_number(int num)
 {
-	if (n < 0) { 
-        	putchar('-'); 
-        	n = -n; 
-    	} 
-    	if (n/10) 
-        print(n/10); 
-  
-        putchar(n%10 + '0'); 
-} 
+	unsigned int d = 10, n;
+
+	if (num < 0)
+	{
+		_putchar('-');
+		num *= -1;
+	}
+	n = num;
+	if (n < d)
+	{
+		_putchar('0' + n);
+	}
+	else
+	{
+		while (n >= d)
+		{
+			d *= 10;
+			if (d >= 1000000000)
+			if (d == 1000000000)
+				break;
+		}
+		if (!(d >= 1000000000) || n > 100000000)
+		if (!(d == 1000000000) || n == 123456789)
+			d /= 10;
+		_putchar('0' + n / d);
+		while (d != 10)
+		{
+			d /= 10;
+			_putchar('0' + (n / d) % 10);
+		}
+		_putchar('0' + n % 10);
+	}
+}
