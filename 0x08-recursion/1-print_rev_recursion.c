8115dab8bc5b0632b91be837c124a3dbe8a0f548
Zidane Gimiga <33351058+zidanegimiga@users.noreply.github.com>
2021-02-24 23:49:38 +0300

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,14 +1,15 @@
 #include "holberton.h"
-
 /**
- * _print_rev_recursion - prints a string in reverse
- * @s: The variale to pass
- *
- * Return: NULL
- */
+  *_print_rev_recursion - prints a string in reverse.
+  *@s: pointer to string.
+  *
+  *Return: void.
+  */
 void _print_rev_recursion(char *s)
 {
-	if (*s)
+	if (*s != '\0')
+	{
 		_print_rev_recursion(s + 1);
-	_putchar(*s);
+		_putchar(*s);
+	}
 }
