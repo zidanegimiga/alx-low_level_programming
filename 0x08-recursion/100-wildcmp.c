87512f35accdda857cc78bf6b1014bdcaf4020b7
Zidane Gimiga <33351058+zidanegimiga@users.noreply.github.com>
2021-02-25 15:31:55 +0300

    Update 100-wildcmp.c

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -7,12 +7,22 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-  unsigned char c1 = *s1, c2 = *s2;
-  if (*s1 < *s2)
-    return(0);
-  if (*s1 > *s2)
-    return(0);
-  if (*s1 == '\0')
+  int i = 0, flag = 0;
+  while(*s1 != '\0' && s2 != '\0')
+  {
+    if(s1 != s2)
+    {
+      flag = 1;
+      break;
+    }
+    i++;
+  }
+  if(flag == 0 && s1 == '\0' && s2 == '\0');
+  {
     return(1);
-  return wildcmp(s1 + 1, s2 + 1);
+  }
+  else
+  {
+    return(0);
+  }
 }
