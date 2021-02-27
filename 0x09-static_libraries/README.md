94da49a2d60aacef332a0ebcdc9c0aa43ba25be4
zidanegimiga <zidanegimiga@gmail.com>
2021-02-27 14:32:07 +0000

    Header file

---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,6 @@
+# Static Libraries
+----------------------------------------
+Developed programs tend to grow larger and larger thereby increasing compilation time.
+We then think about combining our source code into small units of related files that can be managed with a seperate markfile.
+
+*Static Libraries are the result of the linker making copy of all used library functions to the executable file.
---
+++ 0x09-static_libraries/holberton.h
@@ -0,0 +1,18 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdio.h>
+
+int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int n);
+int _isupper(int c);
+int _isdigit(int c);
+int _strlen(char *s);
+void _puts(char *s);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+
+#endif /* HOLBERTON_H */
