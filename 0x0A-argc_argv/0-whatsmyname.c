a8fab6c1a0bae978beccdeafb96c6bc03892c28f
zidanegimiga <zidanegimiga@gmail.com>
2021-03-01 13:31:20 +0000

    A program the prints out its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,9 +7,9 @@
  * @argv: argument vector
  * Return: An integer
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	(void)argc;
 	printf("%s\n", argv[0]);
-	return(0);
+	return (0);
 }
