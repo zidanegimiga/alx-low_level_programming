6c25ad36327c566b1fc48c61f58d637c42018dc7
zidanegimiga <zidanegimiga@gmail.com>
2021-03-01 16:51:28 +0000

    A program that prints the argumnts it recieves

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints arguments passed to it by the cmd
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	int count = 0;
+
+	while (count < argc)
+	{
+		printf("%s\n", argv(count);
+		count++;
+	}
+	return (0);
+}
