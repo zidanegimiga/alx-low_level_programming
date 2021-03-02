266f9ba6b438ef332e72e036eed7b5dcef3304cc
Zidane Gimiga <33351058+zidanegimiga@users.noreply.github.com>
2021-03-02 22:11:21 +0300

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,24 +1,32 @@
-#include "holberton.h"
 #include <stdlib.h>
 
 /**
- * _strdup - returns a pointer to a newly allocated space in memory,
- * which contains a copy of the string given as a parameter
- *
- * @str: string to duplicate
- * Return: char pointer
+ * _strdup - copy
+ * @str: pointer to string
+ * Return: pointer
  */
+
 char *_strdup(char *str)
 {
-	char *sd;
-	unsigned int size = 0, i;
+char *s;
+int i = 0, j;
 
-	if (str == NULL)
-		return (NULL);
-	for (i = 0; str[i] != '\0'; i++)
-		size++;
-	sd = malloc(sizeof(char) * size);
-	for (i = 0; i < size; i++)
-		sd[i] = str[i];
-	return (sd);
+if (!str)
+return (NULL);
+
+while (*(str + i))
+i++;
+i++;
+s = malloc(sizeof(char) * i);
+
+if (s == NULL)
+{
+return (NULL);
+}
+
+for (j = 0; j <= i; j++)
+{
+s[j] = str[j];
+}
+return (s);
 }
