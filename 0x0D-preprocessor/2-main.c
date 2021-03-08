d34abca0554f79e59fdc4cfb4ac9ec5b1215a16c
Zidane Gimiga <33351058+zidanegimiga@users.noreply.github.com>
2021-03-08 16:22:35 +0300

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
-
-/*
- * main - prints the program name
- *
- * Return: 0
- */
+/**
+  *main - prints filename.
+  *
+  *Return: 0.
+  */
 int main(void)
 {
-	printf("%s\n", _FILE_);
+	printf("%s\n", __FILE__);
 	return (0);
 }
