0915e2b73e56b2774883fb7252410f86dfe736eb
Zidane Gimiga <33351058+zidanegimiga@users.noreply.github.com>
2021-03-11 23:45:44 +0300

    Replaced while condition with if

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,27 +1,27 @@
-#include "variadic_functions.h"
-
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
 /**
- * print_numbers - prints numbers, followed by a new line
- *
- * @separator: string to be printed between numbers
- * @n: number of arguments
- * Return: integer result or 0 if n == 0
- */
+  *print_numbers - prints numbers followed by a new line.
+  *@separator: string to be printed between numbers.
+  *@n: number of integers passed to the function.
+  *
+  *Return: void.
+  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list args;
-	unsigned int i = n;
+	unsigned int i;
+	va_list numbers;
 
-	if (n != 0)
+	va_start(numbers, n);
+
+	for (i = 0; i < n; i++)
 	{
-		va_start(args, n);
-		for (; i; i--)
-		{
-			printf("%d", va_arg(args, int));
-			if (i - 1 != 0 && separator != NULL)
-				printf("%s", separator);
-		}
-		printf("\n");
+		printf("%d", va_arg(numbers, int));
+		if (i < n - 1 && separator != NULL)
+			printf("%s", separator);
 	}
-	va_end(args);
+	printf("\n");
+
+	va_end(numbers);
 }
