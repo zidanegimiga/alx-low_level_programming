87204b456684108958f4a26307149c74e2d265ad
zidanegimiga <zidanegimiga@gmail.com>
2021-03-19 17:39:14 +0000

    ..

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_list - prints all the elements of a list
+ *
+ * @h: pointer to a list
+ *
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t node = 0;
+
+	while (h != NULL)
+	{
+		if (h -> str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h -> len, h -> str);
+		nodes++;
+		h = h -> next;
+	}
+	return (nodes);
+}
