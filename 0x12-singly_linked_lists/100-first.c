a2f3bff964db70a4e45989f807ab3db3bb4750f0
Zidane Gimiga <33351058+zidanegimiga@users.noreply.github.com>
2021-03-19 20:46:50 +0300

    A pre - main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * pre_main - prints strings before main
+ *
+ */
+void pre_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
