b171251ff82369e45533e3e21065a10db538e7ff
Zidane Gimiga <33351058+zidanegimiga@users.noreply.github.com>
2021-03-20 16:44:34 +0300

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,11 @@
 #include "lists.h"
-
 /**
- * pre_main - prints strings before main
- *
- */
-void pre_main(void)
+  *print - prints before main
+  *
+  *Return: void.
+  */
+void print(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
 }
