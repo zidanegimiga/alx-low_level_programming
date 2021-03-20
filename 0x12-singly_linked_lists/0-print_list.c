d066eb82b3c67e5ed8a610c5648da439a32ec376
Zidane Gimiga <33351058+zidanegimiga@users.noreply.github.com>
2021-03-20 16:41:18 +0300

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,24 +1,27 @@
-#include "holberton.h"
-
+#include "lists.h"
 /**
- * print_list - prints all the elements of a list
- *
- * @h: pointer to a list
- *
- * Return: number of nodes
- */
+  *print_list - prints all the elements of a list_t.
+  *@h: pointer to list.
+  *
+  *Return: the number of nodes.
+  */
 size_t print_list(const list_t *h)
 {
-	size_t node = 0;
+	int count;
 
+	count = 0;
 	while (h != NULL)
 	{
-		if (h -> str == NULL)
+		if (h->str == NULL)
+		{
 			printf("[0] (nil)\n");
-		else
-			printf("[%u] %s\n", h -> len, h -> str);
-		nodes++;
-		h = h -> next;
+			h = h->next;
+			count++;
+		}
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		count++;
 	}
-	return (nodes);
+
+	return (count);
 }
