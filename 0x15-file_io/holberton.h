08e620274be4435a49e5f029cc02e8d8c71319dd
Zidane Gimiga <33351058+zidanegimiga@users.noreply.github.com>
2021-03-29 20:05:22 +0300

    Header file containing prototypes

---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,12 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+
+#endif /* HOLBERTON_H */
